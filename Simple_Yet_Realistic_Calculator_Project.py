aaa5a6e7fae8949570446c2533ed1b473af785a6
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-02-10 00:17:42 +0800

    Added all the Buttons and Program Height Adjusted

--- Simple_Yet_Realistic_Calculator_Project.py
+++ Simple_Yet_Realistic_Calculator_Project.py
@@ -12,7 +12,7 @@ from tkinter import *
  # Displays the Layout of the Program
 root = Tk()
 root.title("My Simple Yet Fancy Python Calculator")
-root.geometry("570x600+200+300")
+root.geometry("570x630+200+300")
 root.resizable(False,False)
 root.configure(bg = "#17161b")
 
@@ -21,7 +21,7 @@ label_result = Label(root, width = 25, height = 2, text = "", font = ("Comic San
 label_result.pack() 
 
 # Displays the Buttons on the 1st Row of the Program
-Button(root,text="C", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#eb5406").place(x = 10, y = 120)
+Button(root,text="C", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#fe9037").place(x = 10, y = 120)
 Button(root,text="/", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 150, y = 120)
 Button(root,text="%", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 120)
 Button(root,text="*", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 430, y = 120)
@@ -37,6 +37,14 @@ Button(root,text="4", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"
 Button(root,text="5", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 150, y = 320)
 Button(root,text="6", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 320)
 Button(root,text="+", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 430, y = 320)
+
+# Displays the Buttons on the 4th and 5th Row of the Program
+Button(root,text="1", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 10, y = 420)
+Button(root,text="2", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 150, y = 420)
+Button(root,text="3", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 420) 
+Button(root,text="0", width = 11, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 10, y = 520)
+Button(root,text=".", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 520)
+Button(root,text="=", width = 5, height = 3, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#3697f5").place(x = 430, y = 420)
 
 # Function to run the Program
 root.mainloop()
