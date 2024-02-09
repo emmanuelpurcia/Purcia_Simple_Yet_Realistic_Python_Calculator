efec65f38b9547feea28fcfc889a2ab1e9ef93f0
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-02-09 23:18:37 +0800

    Black and Empty Program

--- Simple_Yet_Realistic_Calculator_Project.py
+++ Simple_Yet_Realistic_Calculator_Project.py
@@ -1,6 +1,18 @@
-# Hello! I call this program my "Simple Yet Realistic Calculator" 
+# Hello! I call this program my "Simple Yet Realistic Python Calculator" 
 # I chose to do this as my final project in our Programming Logic and Design Subject because I want to know
 # How most programmers make a calculator using a software.
 # This existing simple calculator project was made by a youtuber named "Parvat Computer Technology"
 
 # pseudocode
+
+# Import Tkinter
+import tkinter
+from tkinter import *
+
+root = Tk()
+root.title("My Simple Yet Realistic Python Calculator")
+root.geometry("570x600+100+200")
+root.resizable(False,False)
+root.configure(bg="#17161b")
+
+root.mainloop()
