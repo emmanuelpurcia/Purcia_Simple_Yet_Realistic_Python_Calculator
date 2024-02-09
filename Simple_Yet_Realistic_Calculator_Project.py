c7d605dbd300abab3ec38ef6bb1b34caf8eba1e5
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-02-09 23:23:22 +0800

    Black and White Empty Program

--- Simple_Yet_Realistic_Calculator_Project.py
+++ Simple_Yet_Realistic_Calculator_Project.py
@@ -15,4 +15,8 @@ root.geometry("570x600+100+200")
 root.resizable(False,False)
 root.configure(bg="#17161b")
 
+
+label_result = Label(root,width = 25, height = 2, text = "", font = ("Comic Sans", 30))
+label_result.pack() 
+
 root.mainloop()
