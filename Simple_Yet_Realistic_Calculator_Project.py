d2f4ca311f1653f88723041cbd4a6b160286d3b4
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-02-09 23:11:06 +0800

    pseudocode

---
+++ Simple_Yet_Realistic_Calculator_Project.py
@@ -0,0 +1,6 @@
+# Hello! I call this program my "Simple Yet Realistic Calculator" 
+# I chose to do this as my final project in our Programming Logic and Design Subject because I want to know
+# How most programmers make a calculator using a software.
+# This existing simple calculator project was made by a youtuber named "Parvat Computer Technology"
+
+# pseudocode
