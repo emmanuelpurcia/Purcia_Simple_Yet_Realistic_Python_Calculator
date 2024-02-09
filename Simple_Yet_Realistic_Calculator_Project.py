8e508a3c361504339b569c3e4a53d54138fb77e5
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-02-09 23:55:42 +0800

    Adjusted Placing of Buttons

--- Simple_Yet_Realistic_Calculator_Project.py
+++ Simple_Yet_Realistic_Calculator_Project.py
@@ -12,19 +12,25 @@ from tkinter import *
  # Displays the Layout of the Program
 root = Tk()
 root.title("My Simple Yet Fancy Python Calculator")
-root.geometry("570x600+100+200")
+root.geometry("570x600+200+300")
 root.resizable(False,False)
-root.configure(bg="#17161b")
+root.configure(bg = "#17161b")
 
-# Function that shows the button you click
-label_result = Label(root,width = 25, height = 2, text = "", font = ("Comic Sans MS", 30))
+# Function that shows the label of the button the user is clicking
+label_result = Label(root, width = 25, height = 2, text = "", font = ("Comic Sans MS", 30))
 label_result.pack() 
 
-# Displays the Buttons on the 1st Row
-Button(root,text="C", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#3697f5").place(x = 10, y = 100)
-Button(root,text="/", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 150, y = 100)
-Button(root,text="%", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 100)
-Button(root,text="*", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 430, y = 100)
+# Displays the Buttons on the 1st Row of the Program
+Button(root,text="C", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#3697f5").place(x = 10, y = 120)
+Button(root,text="/", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 150, y = 120)
+Button(root,text="%", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 120)
+Button(root,text="*", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 430, y = 120)
+
+# Displays the Buttons on the 2nd Row of the Program
+Button(root,text="7", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#3697f5").place(x = 10, y = 220)
+Button(root,text="8", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 150, y = 220)
+Button(root,text="9", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 290, y = 220)
+Button(root,text="-", width = 5, height = 1, font = ("Comic Sans MS", 30, "bold"), bd = 1, fg = "#fff", bg = "#2a2d36").place(x = 430, y = 220)
 
 # Function to run the Program
 root.mainloop()
