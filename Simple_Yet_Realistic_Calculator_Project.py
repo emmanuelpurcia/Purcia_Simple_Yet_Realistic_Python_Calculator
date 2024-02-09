4a8cfec0b366b1fe8adf2450f071a804b786fc29
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-02-09 23:28:35 +0800

    Added C Button

--- Simple_Yet_Realistic_Calculator_Project.py
+++ Simple_Yet_Realistic_Calculator_Project.py
@@ -19,4 +19,6 @@ root.configure(bg="#17161b")
 label_result = Label(root,width = 25, height = 2, text = "", font = ("Comic Sans", 30))
 label_result.pack() 
 
+Button(root,text="C", width = 5, height = 1, font = ("Comic Sans", 30, "bold"), bd = 1, fg = "#fff", bg = "#3697f5").place(x = 10, y = 100)
+
 root.mainloop()
